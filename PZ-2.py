959c0ce4a99c7bd2f6af8290f0b6a4afff7c8a2b
1111000-1000100 <hateeveryone.donttouchme@gmail.com>
2024-09-27 15:15:17 +0300

    .

--- PZ-2.py
+++ PZ-2.py
@@ -1,8 +1,1 @@
-def full_kilobytes(file_size_bytes):
-    kilobytes = file_size_bytes // 1024
-    return kilobytes
 
-file_size = int(input("Введите размер файла в байтах: "))
-
-full_kilobytes = full_kilobytes(file_size)
-print(f"Количество полных килобайтов, занимаемых файлом: {full_kilobytes} кБ")
